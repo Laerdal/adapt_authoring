843b8c6d0c9d57b86d06de8b3c8b42f8125353df
lokesh <lokesh.krishnappa@laerdal.com>
2024-10-24 14:47:54 +0530

    Add Notification Popups for Deprecated Plugins

--- frontend/src/modules/navigation/views/navigationView.js
+++ frontend/src/modules/navigation/views/navigationView.js
@@ -17,6 +17,7 @@ define(function(require){
     },
 
     render: function() {
+      Origin.Notify.alert({ type: 'info', text: '<p style="text-align:left">To streamline our extensions, we will soon disable some that are no longer needed. Below is a list of the extensions being replaced, along with their alternatives. Please note that this will not affect existing courses.</p><br/> <ul style="text-align:left"><li>Laerdal Branching → Branching + Trickle</li><li>Laerdal Spoor → Spoor</li><li>Inline Feedback → Tutor with inline option</li></ul>' });
       var data = this.model ? this.model.toJSON() : null;
       var template = Handlebars.templates[this.constructor.template];
       this.$el.html(template(data));
