ce5c966a465458fdf6012b8187d43864f1542fec
Aahwan verma <vaahwan@gmail.com>
2026-08-18 12:10:39 +0530

    feature/ADAPT-3754 copilot pr comment resolved

--- new-ui-source/src/pages/SetupPage.tsx
+++ new-ui-source/src/pages/SetupPage.tsx
@@ -1,4 +1,4 @@
-﻿import { useSearchParams, useNavigate, useParams } from "react-router-dom";
+import { useSearchParams, useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Suspense, useEffect, useState, useRef, useCallback } from "react";
 import { useAuth } from "../context/AuthContext";
